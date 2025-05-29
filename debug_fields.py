467e78c5ac25aace6d2af2884dc793fdcf5e79a0
RabowNicholas <rabownick123@gmail.com>
2025-05-29 16:06:19 -0600

    remove unneeded files

--- debug_fields.py
+++
@@ -1,9 +0,0 @@
-from pdfrw import PdfReader
-
-pdf = PdfReader("app/templates/EE-1 DRAFT.pdf")
-for page in pdf.pages:
-    annotations = page.Annots
-    if annotations:
-        for annot in annotations:
-            if annot.Subtype == "/Widget" and annot.T:
-                print(annot.T)
