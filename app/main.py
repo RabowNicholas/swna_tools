983e6f851d8e5ec6bbc38b96b659cdec0d03587e
RabowNicholas <rabownick123@gmail.com>
2025-06-17 08:54:23 -0600

    en11a rework

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-import tkinter as tk
-from app.gui import PrefillGUI
+import toga
+from forms.en11a_view import EN11AView
+
+
+class SWNAToolsApp(toga.App):
+    def __init__(self):
+        super().__init__("SWNA Tools", "org.swna.tools")
+
+    def startup(self):
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.en11a_view = EN11AView(app=self)
+        self.main_window.content = self.en11a_view.main_box
+        self.main_window.show()
+
+
+def main():
+    return SWNAToolsApp()
+
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = PrefillGUI(root)
-    root.mainloop()
+    main().main_loop()
